ae8ff450c277fd435734e6830ca6c85995e7b566
Daniel Stutzman <dtstutz@gmail.com>
2013-03-12 21:26:06 -0600

    Added authentication with Google Plus.

--- web-app.rb
+++ web-app.rb
@@ -2,7 +2,18 @@ require 'sinatra'
 require 'pry'
 require 'json'
 require 'haml'
+require 'omniauth'
+require 'omniauth-google-oauth2'
+require 'yaml'
 require './get_trace_for.rb'
+
+config_path = File.join(File.dirname(__FILE__), 'config.yaml')
+CONFIG = YAML.load_file(config_path)
+
+use Rack::Session::Cookie, {
+  :key => 'rack.session',
+  :secret => CONFIG['COOKIE_SIGNING_SECRET'],
+}
 
 set :port, 4567
 set :public_folder, 'public'
@@ -18,3 +29,38 @@ post '/' do
   @trace = JSON.dump(get_trace_for(user_code))
   haml :index
 end
+
+use OmniAuth::Builder do
+  provider :google_oauth2, CONFIG['GOOGLE_KEY'], CONFIG['GOOGLE_SECRET'], {
+    :scope => 'https://www.googleapis.com/auth/plus.me',
+    :access_type => 'online',
+  }
+end
+
+# Example callback:
+#
+# {"provider"=>"google_oauth2",
+#  "uid"=>"112826277336975923063",
+#  "info"=>{},
+#  "credentials"=>
+#   {"token"=>"ya29.AHES6ZRDLUipo8HB5wLy7MoO81vjath9i7Wx-4nI-duhXyE",
+#    "expires_at"=>1363146592,
+#    "expires"=>true},
+#  "extra"=>{"raw_info"=>{"id"=>"112826277336975923063"}}}
+#
+get '/auth/google_oauth2/callback' do
+  response = request.env['omniauth.auth']
+  uid = response['uid']
+  if CONFIG['AUTHORIZED_GOOGLE_PLUS_UIDS'].include?(uid)
+    'success'
+  else
+    'failed'
+  end
+end
+
+get '/auth/failure' do
+  message = params['message']
+  message
+end
+
+# http://localhost:4567/auth/google_oauth2
