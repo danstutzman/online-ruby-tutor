aa684af90185b8a3f6d3586a3a3e97e41360691f
Daniel Stutzman <dtstutz@gmail.com>
2013-03-19 13:30:14 -0600

    Different home directory on prod.

--- unicorn.rb
+++ unicorn.rb
@@ -1,6 +1,6 @@
 # set path to app that will be used to configure unicorn, 
 # note the trailing slash in this example
-@dir = "/home/ubuntu/online-ruby-tutor/"
+@dir = "/root/online-ruby-tutor/"
 
 worker_processes 1
 working_directory @dir
